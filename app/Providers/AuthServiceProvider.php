560be4c6a26fab5ca580d3cd4bd3084ab618f42d
jostinOG <Jostinfabian14@gmail.com>
2022-10-05 12:48:43 +0200

    The email sent has been customized.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,6 +27,13 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        VerifyEmail::toMailUsing(function ($email, $url) {
+            return (new MailMessage)
+                ->subject(Lang::get('Verify Email Address!!'))
+                ->line(Lang::get('Please click the button below to verify your email address.'))
+                ->action(Lang::get('Verify Email Address'), $url)
+                ->line(Lang::get('If you did not create an account, no further action is required.'));
+        });
 
         //
     }
