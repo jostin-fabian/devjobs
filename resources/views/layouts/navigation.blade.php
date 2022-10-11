71407c9baa8456364f86cb94b589caede4092f44
jostinOG <Jostinfabian14@gmail.com>
2022-10-11 15:33:06 +0200

    Hiding Notifications for Devs and Preventing Access to Notifications

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,15 +11,20 @@
                 </div>
                 {{-----------------Authenticated user----------------------------}}
                 @auth
-                    <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-nav-link :href="route('vacancies.index')" :active="request()->routeIs('vacancies.index')">
-                            {{ __('My Vacancies') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('vacancies.create')" :active="request()->routeIs('vacancies.create')">
-                            {{ __('Create Vacancy') }}
-                        </x-nav-link>
-                    </div>
+                    @can('create',App\Models\Vacancy::class)
+                        <!-- Navigation Links -->
+
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <x-nav-link :href="route('vacancies.index')"
+                                        :active="request()->routeIs('vacancies.index')">
+                                {{ __('My Vacancies') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('vacancies.create')"
+                                        :active="request()->routeIs('vacancies.create')">
+                                {{ __('Create Vacancy') }}
+                            </x-nav-link>
+                        </div>
+                    @endcan
                 @endauth
 
             </div>
@@ -27,12 +32,13 @@
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 @auth
-                    @if(auth()->user()->role===2)
+                    @can('create',App\Models\Vacancy::class)
                         <a class="mr-2 w-7 h-7 bg-indigo-600 hover:bg-indigo-800 rounded-full flex flex-col justify-center items-center text-sm font-extrabold text-white"
                            href="{{route('notifications')}}">
                             {{auth()->user()->unreadNotifications->count()}}
                         </a>
-                    @endif
+                    @endcan
+
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/dashboard', [VacancyController::class, 'index'])->middleware(['auth', 'verified'])->name('vacancies.index');
+Route::get('/dashboard', [VacancyController::class, 'index'])->middleware(['auth', 'verified', 'recruiter.role'])->name('vacancies.index');
 Route::get('/vacancies/create', [VacancyController::class, 'create'])->middleware(['auth', 'verified'])->name('vacancies.create');
 Route::get('/vacancies/{vacancy}/edit', [VacancyController::class, 'edit'])->middleware(['auth', 'verified'])->name('vacancies.edit');
 Route::get('/vacancies/{vacancy}', [VacancyController::class, 'show'])->name('vacancies.show');
