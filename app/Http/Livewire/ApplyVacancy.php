5d3f2fb1b5fecb45a04d0f9331f7a850b17a9538
jostinOG <Jostinfabian14@gmail.com>
2022-10-11 11:48:41 +0200

    236. Storing CV or Resume

--- app/Http/Livewire/ApplyVacancy.php
+++ app/Http/Livewire/ApplyVacancy.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Candidate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ApplyVacancy extends Component
 {
     public $cv;//cv
+    use WithFileUploads;
+
     protected $rules = [
         'cv' => 'required|mimes:pdf'
     ];
@@ -15,8 +19,22 @@ class ApplyVacancy extends Component
     {
         $this->validate();
         //Store cv in disc
+        $cv = $this->cv->store('public/cv');
+        $data['cv'] = str_replace('public/cv/', '', $cv);
         //create vacancy
+
+        Candidate::create([
+            'cv' => $data['title'],
+            'vacancy_id' => $data['category'],
+            'company' => $data['company'],
+            'last_day' => $data['last_day'],
+            'description' => $data['description'],
+            'image' => $data['image'],
+            'user_id' => auth()->user()->id,
+        ]);
         //Create the notification and send the email
+        session()->flash('message', 'The vacancy was correctly published');
+
         //Show the user an ok message
 
     }
