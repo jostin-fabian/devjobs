7af2c94a9fd32f6645ee7d99f1deba5e207d0327
jostinOG <Jostinfabian14@gmail.com>
2022-10-11 11:43:42 +0200

    Form Validation

--- app/Http/Livewire/ApplyVacancy.php
+++ app/Http/Livewire/ApplyVacancy.php
@@ -6,6 +6,21 @@ use Livewire\Component;
 
 class ApplyVacancy extends Component
 {
+    public $cv;//cv
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+
+    public function applyTo()
+    {
+        $this->validate();
+        //Store cv in disc
+        //create vacancy
+        //Create the notification and send the email
+        //Show the user an ok message
+
+    }
+
     public function render()
     {
         return view('livewire.apply-vacancy');
--- resources/views/livewire/apply-vacancy.blade.php
+++ resources/views/livewire/apply-vacancy.blade.php
@@ -1,14 +1,18 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
     <h3 class="text-center text-2xl font-bold my-4">Apply to this Vacancy</h3>
-    <form class="w-96 mt-5" role="form" action="#" method="post" enctype="multipart/form-data">
+    <form wire:submit.prevent="applyTo" class="w-96 mt-5" novalidate>
         <div class="mb-4">
             {{--CV--}}
             <x-input-label for="cv" :value="__('Resume or CV (PDF)')"/>
-            <x-text-input id="cv" class="block mt-1 w-full" type="file" name="cv" accept=".pdf"/>
+            <x-text-input id="cv" class="block mt-1 w-full" type="file" wire:model="cv" accept=".pdf"/>
         </div>
-        <x-primary-button class="w-full justify-center">
-            {{__('Apply to')}}
-        </x-primary-button>
+        @error('cv')
+        <livewire:show-alert :message="$message">
+            @enderror
+            <x-primary-button class="my-5">
+                {{__('Apply to')}}
+            </x-primary-button>
+
 
     </form>
 </div>
