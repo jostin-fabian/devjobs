1d62e9a64551b5ba75f16c4510946f87e034ea9c
jostinOG <Jostinfabian14@gmail.com>
2022-10-14 10:26:33 +0200

    First steps to create a search engine

---
+++ app/Http/Livewire/FilterVacancies.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Category;
+use App\Models\Salary;
+use Livewire\Component;
+
+class FilterVacancies extends Component
+{
+    public $term;// term for filtering
+    public $category;//category for filtering
+    public $salary;//salary for filtering
+
+    //read form data
+    public function readFormData()
+    {
+        dd('Searching for');
+
+    }
+
+    public function render()
+    {
+        $salaries = Salary::all();
+        $categories = Category::all();
+        return view('livewire.filter-vacancies', [
+            'salaries' => $salaries,
+            'categories' => $categories
+        ]);
+    }
+}
--- resources/views/livewire/home-vacancies.blade.php
+++ resources/views/livewire/home-vacancies.blade.php
@@ -1,4 +1,5 @@
 <div>
+    <livewire:filter-vacancies/>
     <div class="py-12">
         <div class="max-w-7xl mx-auto">
             <h3 class="font-extrabold text-4xl text-gray-700 mb-12">Our available vacancies </h3>
