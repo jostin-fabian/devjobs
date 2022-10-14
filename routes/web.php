be432729a6f6709dadb1541ef10874c2ac7b4c8d
jostinOG <Jostinfabian14@gmail.com>
2022-10-14 09:54:53 +0200

    Creating the home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CandidateController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\VacancyController;
 use Illuminate\Support\Facades\Route;
@@ -16,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('/dashboard', [VacancyController::class, 'index'])->middleware(['auth', 'verified', 'recruiter.role'])->name('vacancies.index');
 Route::get('/vacancies/create', [VacancyController::class, 'create'])->middleware(['auth', 'verified'])->name('vacancies.create');
